80bd06ca1cd0c556347baae8e5fca01f88c664a6
legrandgrand <alexandre.danty@gmail.com>
2019-02-20 11:09:49 +0100

    [Log] Added loggers on the project. XML DOCTYPE fixes

--- src/main/java/Cdb.java
+++ src/main/java/Cdb.java
@@ -1,9 +1,16 @@
 import controller.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import service.ServiceCompany;
 import service.ServiceComputer;
+
 import view.View;
 
 public class Cdb {
+
+  private static final Logger logger = LoggerFactory.getLogger(Cdb.class);
 
   /**
    * The main method.
@@ -11,6 +18,7 @@ public class Cdb {
    * @param args the arguments
    */
   public static void main(String[] args) {
+    logger.debug("Starting program");
     ServiceComputer s1 = ServiceComputer.getInstance();
     ServiceCompany s2 = ServiceCompany.getInstance();
     Controller c = new Controller(s1, s2);
